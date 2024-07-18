a0b3c482f30d941996a4269cd25413898a3876a6
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-07-17 20:04:11 -0400

    done

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const colors = express.Router();
-const { getAllColors } = require("../queries/color");
+const { checkName, checkBoolean } = require("../validations/checkColors.js");
+const { getAllColors, getColor, createColor } = require("../queries/color");
 
-// Index
+// INDEX
 colors.get("/", async (req, res) => {
   const allColors = await getAllColors();
   if (allColors[0]) {
     res.status(200).json(allColors);
   } else {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+colors.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const color = await getColor(id);
+  if (color) {
+    res.json(color);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+colors.post("/", checkName, checkBoolean, async (req, res) => {
+  try {
+    const color = await createColor(req.body);
+    res.json(color);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
